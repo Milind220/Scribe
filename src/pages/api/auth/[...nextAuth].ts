eecd3cc4e0c608aa4098b93dc480e0862a48ca39
Milind Sharma <milindsharma8@gmail.com>
2025-04-13 18:19:56 +0800

    Add type extension and fix Auth handler

--- src/pages/api/auth/[...nextAuth].ts
+++ src/pages/api/auth/[...nextAuth].ts
@@ -2,29 +2,27 @@ import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 
 const handler = NextAuth({
-    providers: [
-        TwitterProvider({
-            clientId: process.env.TWITTER_CLIENT_ID as string,
-            clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-            version: "2.0",    // Twitter API version
-        })
-    ],
-    callbacks: {
-      async jwt({ token, account }) {
-        if (account?.provider === "twitter") {
-          token.accessToken = account.access_token;
-          token.refreshToken = account.refresh_token;
-          token.accessTokenExpires = account.expires_at;
-        }
-        return token;
-      },
-      async session({ session, token }) {
-        if (session.user) {
-          // TODO: Add the access token to the session
-        }
-        return session;
+  providers: [
+      TwitterProvider({
+          clientId: process.env.TWITTER_CLIENT_ID as string,
+          clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+          version: "2.0",    // Twitter API version
+      })
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.provider === "twitter") {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.accessTokenExpires = account.expires_at;
       }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken as string;
+      return session;
     }
+  }
 })
 
 export { handler as GET, handler as POST }
