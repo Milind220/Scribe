1f2bbb0942219c4dd814ceabdac94b4e4cfba9d8
Milind Sharma <milindsharma8@gmail.com>
2025-04-27 00:20:29 +0800

    Fix the right file

--- src/pages/api/create-checkout-session.ts
+++ src/pages/api/create-checkout-session.ts
@@ -5,7 +5,7 @@ import { StripeCheckoutSessionResponse } from '@/types/stripe';
 import { createClient } from '@supabase/supabase-js';
 import { authOptions } from './auth/[...nextauth]';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const appUrl = process.env.APP_URL || 'http://localhost:3000';
